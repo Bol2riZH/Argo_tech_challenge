630a8702257b8a61be7ca4144e390e59f803123c
Matthieu <reungoat.matthieu@gmail.com>
2022-12-16 15:47:28 +0100

    - create backend architecture - create a db > mongodb - init server

---
+++ backend/app.js
@@ -0,0 +1,13 @@
+'use strict';
+
+require('mongodb');
+const express = require('express');
+const app = express();
+
+app.use(express.json());
+
+// const crewRoutes = require('routes/crew');
+
+// app.use('/api/', crewRoutes);
+
+module.exports = app;
---
+++ backend/mongodb.js
@@ -0,0 +1,13 @@
+'use strict';
+
+const mongoose = require('mongoose');
+
+mongoose.connect(
+  `mongodb+srv://Matthieu:DSqvdX4fJw7D7cdQ@argonautes.sfdnyhk.mongodb.net/
+`,
+  { useNewUrlParser: true, useUnifiedTopology: true },
+  (err) => {
+    if (!err) console.log('MongoDB connected');
+    else console.error('connection error: ' + err);
+  }
+);
