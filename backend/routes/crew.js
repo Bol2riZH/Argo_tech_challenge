4f79f00dd7c1038e54c05db29cf594681cbc9d48
Matthieu <reungoat.matthieu@gmail.com>
2022-12-16 16:12:22 +0100

    create routes - init controllers

--- backend/routes/crew.js
+++ backend/routes/crew.js
@@ -1,1 +1,11 @@
 'use strict';
+
+const express = require('express');
+const router = express.Router();
+
+const crewCtrl = require('../controllers/crew');
+
+router.get('/', crewCtrl.getAll);
+router.get('/', crewCtrl.addCrewMember);
+
+module.exports = router;
